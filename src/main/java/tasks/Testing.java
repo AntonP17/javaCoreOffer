06c30a5df2893d573a27b7cb2a0128165eb7c42c
AntonP17 <pokrovskii.anton@mail.ru>
2025-03-23 10:56:09 +0400

    create customStringBulder with pattern snapshot

--- src/main/java/tasks/Testing.java
+++ src/main/java/tasks/Testing.java
@@ -2,6 +2,18 @@ package tasks;
 
 public class Testing {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        customStringBuilder stringBuilder = new customStringBuilder();
+        StringBuilderCaretaker caretaker = new StringBuilderCaretaker();
+
+        stringBuilder.append("Hello , how are you ..");
+        caretaker.saveState(stringBuilder);
+
+        stringBuilder.append("I am fine , how about you ?");
+        System.out.println("Current state: " + stringBuilder.getText());
+
+        caretaker.undo(stringBuilder);
+        System.out.println("Restored state: " + stringBuilder.getText());
+
     }
 }
+
