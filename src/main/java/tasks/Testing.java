cb141e3004939867dcefdd7518a68055d846523c
AntonP17 <pokrovskii.anton@mail.ru>
2025-03-27 22:02:34 +0300

    stream with collectors , Pipeline

--- src/main/java/tasks/Testing.java
+++ src/main/java/tasks/Testing.java
@@ -1,7 +1,33 @@
 package tasks;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+
 public class Testing {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        List<Order> orders = List.of(
+                new Order("Laptop", 1200.0),
+                new Order("Smartphone", 800.0),
+                new Order("Laptop", 1500.0),
+                new Order("Tablet", 500.0),
+                new Order("Smartphone", 900.0)
+        );
+
+        List<Map.Entry<String, Double>> topProducts =
+                orders.stream()
+                        .collect(Collectors.groupingBy(
+                                Order::getProduct,
+                                Collectors.summingDouble(Order::getCost)))
+                        .entrySet()
+                        .stream()
+                        .sorted(Map.Entry.<String,Double>comparingByValue().reversed())
+                        .limit(3)
+                        .toList();
+
+        System.out.println("Three some expensive products: " );
+        topProducts.forEach(entry ->
+                System.out.println(entry.getKey() + ": " + entry.getValue()));
     }
 }
